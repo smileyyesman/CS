63ad48093f85bdccab1fa72cf8017854c78a6ccf
LEE <LEE@LEE>
2023-03-20 22:07:28 +0900

    초기 커밋.

---
+++ MyTetris/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using static Tetriminos;
+
+enum STARTSELECT
+{
+    GAMESELECT,
+    NONESELECT
+}
+
+class Program
+{
+    public static void Ingame()
+    {
+        Map Map = new Map();
+        Map.CreateMap();
+        Tetriminos Tetriminos = new Tetriminos();
+        while (true)
+        {
+            TETRIMINOS mTetriminos = new TETRIMINOS();
+            int[] ArrNum = Tetriminos.GetGroup();
+            int x;
+            int y;
+            int newx;
+            int newy;
+            for (int i = 0; i < 7; i++)
+            {
+                GoToxy(12, 0);
+                mTetriminos = Tetriminos.CreateTetriminos(ArrNum[i]);
+                (x, y) = Console.GetCursorPosition();
+                Tetriminos.DrawTetriminos(mTetriminos, x, y);
+                (newx, newy) = Tetriminos.MoveTetriminos(x, y);
+                GoToxy(newx, newy);
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public static void GoToxy(int x, int y)
+    {
+        Console.SetCursorPosition(x, y);
+    }
+
+    static void Main(string[] args)
+    {
+        Commend GameSentence = new Commend();
+        STARTSELECT StartSelect = new STARTSELECT();
+
+        StartSelect = STARTSELECT.NONESELECT;
+        while (true)
+        {
+            if (StartSelect == STARTSELECT.NONESELECT)
+            {
+                GameSentence.StartMessage();
+                if (ConsoleKey.Enter == Console.ReadKey().Key)
+                {
+                    StartSelect = STARTSELECT.GAMESELECT;
+                }
+            }
+
+            else
+            {
+                Console.Clear();
+                Ingame();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Practice4/Inven.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+class Inven
+{
+    Item[] ArrItem;
+    int X;
+    int Y;
+    int SelectIndex = 0;
+    public Inven(int _X, int _Y)
+    {
+        if (1 > _X || 1 > _Y)
+        {
+            _X = 1;
+            _Y = 1;
+        }
+        X = _X;
+        Y = _Y;
+        ArrItem = new Item[_X * _Y];
+    }
+
+    public void Render()
+    {
+        for (int i = 0; i < ArrItem.Length; i++)
+        {
+            if (i != 0 && 0 == i % X)
+            {
+                Console.WriteLine("");
+            }
+            
+            if (SelectIndex == i)
+            {
+                Console.Write("▣");
+            }
+            else if (null == ArrItem[i])
+            {
+                Console.Write("□");
+            }
+            else
+            {
+                Console.Write("■");
+            }            
+        }
+
+        while (true)
+        {
+            ConsoleKeyInfo KeyInfo = Console.ReadKey();
+
+            switch (KeyInfo.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    if (SelectIndex == 0)
+                    {
+                        SelectIndex = X * Y - 1;
+                        return;
+                    }
+                    else
+                    {
+                        SelectIndex -= 1;
+                        return;
+                    }
+                case ConsoleKey.RightArrow:
+                    if (SelectIndex == X * Y - 1)
+                    {
+                        SelectIndex = 0;
+                        return;
+                    }
+                    else
+                    {
+                        SelectIndex += 1;
+                        return;
+                    }
+                case ConsoleKey.UpArrow:
+                    if (SelectIndex < X)
+                    {
+                        SelectIndex += X * (Y - 1);
+                        return;
+                    }
+                    else
+                    {
+                        SelectIndex -= X;
+                        return;
+                    }
+
+                case ConsoleKey.DownArrow:
+                    if (SelectIndex >= X * (Y - 1))
+                    {
+                        SelectIndex -= X * (Y - 1);
+                        return;
+                    }
+                    else
+                    {
+                        SelectIndex += X;
+                        return;
+                    }
+                default:
+                    break;
+            }
+
+        }
+    }   
+
+    public void ItemIn(Item _Item)
+    {
+        for (int i = 0; i < ArrItem.Length; i++)
+        {
+            if (null == ArrItem[i])
+            {
+                ArrItem[i] = _Item;
+                break;
+            }
+        }
+    }
+
+    public void ItemIn(Item _Item, int _Index)
+    {
+        if (null != ArrItem[_Index])
+        {
+            return;
+        }
+        ArrItem[_Index] = _Item;
+    }
+}
