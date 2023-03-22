82c92f4d6d3b7157a3ba1af3baa80d9e1800bb79
LEE <LEE@LEE>
2023-03-22 23:07:14 +0900

    Revert "Revert "초기 커밋.""
    This reverts commit f866c9ffffc5acbe361fdedd68c40c93400587bb.

---
+++ Practice3/Program.cs
@@ -0,0 +1,48 @@
+﻿enum TYPE
+{
+    ATT,
+    FIRE,
+    WATER
+}
+class Player
+{
+    
+    int ATTDef = 3;
+    int FireDef = 4;
+    int WaterDef = 5;
+    int HP = 50;
+
+    public void Damage(int _Dmg)
+    {
+        HP -= _Dmg;
+    }
+    public void Damage(int _Dmg, TYPE _type)
+    {
+        switch (_type)
+        {
+            case TYPE.ATT:
+                _Dmg -= ATTDef;
+                break;
+            case TYPE.FIRE:
+                _Dmg -= FireDef;
+                break;
+            case TYPE.WATER:
+                _Dmg -= WaterDef;
+                break;
+            default:
+                break;
+        }
+        Damage(_Dmg);
+
+    }
+}
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Player NewPlayer = new Player();
+        NewPlayer.Damage(10, TYPE.ATT);
+    }
+}
